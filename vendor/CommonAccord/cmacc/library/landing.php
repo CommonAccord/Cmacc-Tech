c5a53ff44f5f07b675cdeeb52121e084020511c2
James Hazard <james.g.hazard@gmail.com>
2015-07-08 17:14:46 +0200

    stocking the Bare with a bare app

---
+++ vendor/CommonAccord/cmacc/library/landing.php
@@ -0,0 +1,42 @@
+<?php
+/*require('./vendor/autoload.php'); */
+ini_set("allow_url_include", true);
+include("header.php");
+
+?>
+
+<body>
+  <div class="container" id ="containerHome" style="padding:50px">
+    <div class="row">
+     <div class = "col-xs-12">
+     <p align="center"><img src="<?php echo ASSETS_PATH; ?>/cmacc-trans.png" style="width:65%" /></p>
+<br/>
+<hr>
+<h1 class="title">Decentralized Law</h1>
+<hr>
+  <h3 class="title">A legal system for the 21st Century</h4>
+<br/><br/>
+
+</div></div>
+<table><tr><td valign=top>       
+
+<h4 class="subtitle"><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=>The Documents</a>.</h4>
+        
+  </td><td valign=top> &emsp; </td>
+
+<td valign=top align=right width=30%>
+
+<a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096">Tweets by @CommonAccord</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+</td></tr></table>
+</div>
+</div>
+
+    </div> <!--end containerHome !-->
+     
+<div
+  </div><!--end container !-->
+
+
+</body><!-- end body !-->
