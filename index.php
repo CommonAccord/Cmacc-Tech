5d47e2a3bccdc4f9e23d8adc53bf7a668459349e
James Hazard <james.g.hazard@gmail.com>
2015-07-09 08:09:22 +0200

    Adding Z/ to give something to test by.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . LIB_PATH . DI
 
 DEFINE('ASSETS_PATH', 'visual');  // Create global variable to our css and js assets
 
-DEFINE('URLFORREPO', 'https://github.com/CommonAccord/Site-My/');	// The repo home
+DEFINE('URLFORREPO', 'https://github.com/CommonAccord/Site-Bare/');	// The repo home
 
 DEFINE('URLFORDOCSINREPO', URLFORREPO . "/blob/master/Doc/");	// Docs in the repo
 
