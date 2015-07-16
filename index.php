2cd56a65ae7185256b6a956aee43667350892648
James Hazard <james.g.hazard@gmail.com>
2015-07-16 14:44:58 +0200

    update - but needs to be brought up to current with the compliance

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-// CHANGE - URLFOR REPO - and in /vendor/CommonAccord/cmacc/libary/cmacc_helpers.php - edit titles etc.
+// CHANGE - URLFOR REPO - and in /vendor/CommonAccord/cmacc/library/cmacc_helpers.php - edit titles etc.
 // 
 
 DEFINE('ROOT' ,dirname(__FILE__) ); // Root directory of site
@@ -15,7 +15,6 @@ DEFINE('URLFORREPO', 'https://github.com/CommonAccord/Site-Bare/');	// The repo 
 DEFINE('URLFORDOCSINREPO', URLFORREPO . "/blob/master/Doc/");	// Docs in the repo
 
 DEFINE('TEXTEDITWINDOWSIZE', 'cols=120 rows=30'); //Sets the size of the text edit window in both edit.php and openedit.php
-
 DEFINE('TEXTEDITAREASTYLE', 'font-size: 16px; padding:10px;'); //Sets the size of the text edit window in both edit.php and openedit.php
 
 include("cmacc_helpers.php");
